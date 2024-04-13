c7764370fbf71c602f783243cea9e782558b78d5
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-04-13 15:08:43 -0400

    commit
    wk2 React state lesson, part 2

--- nucampsitereactwk1/src/App.js
+++ nucampsitereactwk1/src/App.js
@@ -1,13 +1,14 @@
 import Header from './components/Header';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
