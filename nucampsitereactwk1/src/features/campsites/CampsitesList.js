6ec975457c8ffeeb1ebefc0f9beefc1642ce8d88
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-04-13 17:04:43 -0400

    committ
    react week 2 state exercises part 3 - this broke the site - and can't figure out where the problem is

--- nucampsitereactwk1/src/features/campsites/CampsitesList.js
+++ nucampsitereactwk1/src/features/campsites/CampsitesList.js
@@ -1,8 +1,11 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from './campsitesSlice';
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
 const CampsitesList = () => {
+    const campsites = selectAllCampsites();
+
     return (
         <Row className="ms-auto">
             {CAMPSITES.map((campsite)=> {
